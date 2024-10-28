595bfdddcc708f027a3973c133a6ed617c157223
Obinna Nwachukwu <75106868+OBIGOD3931@users.noreply.github.com>
2024-10-28 20:18:03 +0100

    Update prediction.py
    changed model

--- prediction.py
+++ prediction.py
@@ -49,7 +49,7 @@ def app():
         unsafe_allow_html=True
     )
     # Load the trained Random Forest model
-    model = joblib.load('final_model1.pkl') 
+    model = joblib.load('final_model2.pkl') 
 
     print(model.get_params())  
     print(model._sklearn_version)  
