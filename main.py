476676de32da815b119e29c61f7f372cde79b266
Obinna Nwachukwu <75106868+OBIGOD3931@users.noreply.github.com>
2024-11-08 02:35:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def display_auth_form():
                 st.rerun()  # Rerun to reflect logout state
         else:
             # Display login form
-            name, authentication_status, username = authenticator.login("Login", location="sidebar")
+            name, authentication_status, username = authenticator.login("Login", "sidebar")
             
 
             # Check the authentication status 
