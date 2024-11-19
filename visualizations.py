9d521c58a02feef60ae264d36458221b09e667f3
Obinna Nwachukwu <75106868+OBIGOD3931@users.noreply.github.com>
2024-11-19 02:25:10 -0800

    Update visualizations.py

--- visualizations.py
+++ visualizations.py
@@ -162,81 +162,81 @@ def app():
                 labels={'Group Name': 'Terrorist Group Name', 'Percentage': 'Percentage of terrorist activities'})
 
 
-   st.markdown(
-    """
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
-        
-        .visualization-header {
-            background-color: none;
-            padding: 10px;
-            border-radius: 5px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            font-size: 40px;
-            font-weight: bold;
-            color: white; 
-            font-family: 'Roboto', sans-serif;
-        }
-        .visualization-header span {
-            font-size: 24px;
-            font-weight: bold;
-            color: white; 
-        }
-        .visualization-text {
-            padding: 10px;
-            width: 90%;
-            font-size: 20px;
-            font-weight: bold;
-            color: white; 
-        }
-        .paragraph {
-            color: black; 
-            background-color: white;
-            padding: 20px;
-            margin: 20px;
-            border-radius: 15px;
-        } 
-        
-        .st-emotion-cache-asc41u.e1nzilvr2 {
-            font-size: 18px;
-            color: white;
-            font-weight: bold;
-            margin-bottom: 5px;
-        }
-    
-        .st-emotion-cache-vdokb0.e1nzilvr4 {
-            font-size: 12px;
-            color: white;
-            font-weight: bold;
-            margin-top: 5px;
-        }
-    
-        .st-emotion-cache-z1fhwk.e1nzilvr4 {
-            font-size: 14px;
-            color: white;
-            font-weight: bold;
-            margin-bottom: 10px;
-        }
-
-    </style>
-    <div class="visualization-header">
-        <h1 style='color:#a5abab;'>Visualization</h1>
-    </div>
-
-    <div class="visualization-text">
-        <span>Visualization of historical data</span>
-        <div class="paragraph">
-            <p style="font-size: 20px; text-align: justify;">
-                Explore interactive visualizations highlighting key areas at risk of terrorist attacks in Nigeria. 
-                Our dynamic maps and data-driven charts provide a clear view of past incidents and potential coordinates.
-                Use these insights to better understand the distribution and frequency of attacks, which will help inform preventive measures and strategic planning.
-                Navigate through the visuals to stay informed and proactive in enhancing security within vulnerable regions.
-            </p>
-        </div>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+    st.markdown(
+       """
+       <style>
+           @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+           
+           .visualization-header {
+               background-color: none;
+               padding: 10px;
+               border-radius: 5px;
+               box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+               font-size: 40px;
+               font-weight: bold;
+               color: white; 
+               font-family: 'Roboto', sans-serif;
+           }
+           .visualization-header span {
+               font-size: 24px;
+               font-weight: bold;
+               color: white; 
+           }
+           .visualization-text {
+               padding: 10px;
+               width: 90%;
+               font-size: 20px;
+               font-weight: bold;
+               color: white; 
+           }
+           .paragraph {
+               color: black; 
+               background-color: white;
+               padding: 20px;
+               margin: 20px;
+               border-radius: 15px;
+           } 
+           
+           .st-emotion-cache-asc41u.e1nzilvr2 {
+               font-size: 18px;
+               color: white;
+               font-weight: bold;
+               margin-bottom: 5px;
+           }
+       
+           .st-emotion-cache-vdokb0.e1nzilvr4 {
+               font-size: 12px;
+               color: white;
+               font-weight: bold;
+               margin-top: 5px;
+           }
+       
+           .st-emotion-cache-z1fhwk.e1nzilvr4 {
+               font-size: 14px;
+               color: white;
+               font-weight: bold;
+               margin-bottom: 10px;
+           }
+   
+       </style>
+       <div class="visualization-header">
+           <h1 style='color:#a5abab;'>Visualization</h1>
+       </div>
+   
+       <div class="visualization-text">
+           <span>Visualization of historical data</span>
+           <div class="paragraph">
+               <p style="font-size: 20px; text-align: justify;">
+                   Explore interactive visualizations highlighting key areas at risk of terrorist attacks in Nigeria. 
+                   Our dynamic maps and data-driven charts provide a clear view of past incidents and potential coordinates.
+                   Use these insights to better understand the distribution and frequency of attacks, which will help inform preventive measures and strategic planning.
+                   Navigate through the visuals to stay informed and proactive in enhancing security within vulnerable regions.
+               </p>
+           </div>
+       </div>
+       """,
+       unsafe_allow_html=True
+   )
 
 
     # plotly figures and interpretations
