fcd5288a0acaa489f8ffbf49c732807639cb4a52
Obinna Nwachukwu <75106868+OBIGOD3931@users.noreply.github.com>
2024-11-23 05:28:05 +0100

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -175,11 +175,16 @@ def app():
 
 
             # Check if the probability exceeds 60%
-            if probability > 0.6:
+            if probability >= 0.5:
+                # Display in red
+                st.markdown(f"<span style='color:red;'>The probability of a terrorist attack in {state} is: {probability:.2%}.</span>", unsafe_allow_html=True)
+                st.warning("This is a moderate-risk situation. It is advisable for the security agency to increase vigilance, monitor the situation closely, and implement preemptive measures to reduce the likelihood of an attack.")
+
+            elif probability >= 0.6:
                 # Display in red
                 st.markdown(f"<span style='color:red;'>The probability of a terrorist attack in {state} is: {probability:.2%}.</span>", unsafe_allow_html=True)
                 st.warning("This is a high-risk situation. It is recommended that the security agency take appropriate precautionary measures "
-                    "to ensure maximum security and mitigation of a possible attack..")
+                    "to ensure maximum security and mitigation of a possible attack.")
             elif probability > 0.8:
                 st.markdown(f"<span style='color:red;'>The probability of a terrorist attack in {state} is: {probability:.2%}.</span>", unsafe_allow_html=True)
                 st.warning("This is a very high-risk situation with low uncertainty. It is strongly recommended that the security agency take appropriate precautionary measures "
