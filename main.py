16c957b723ce35a721ab41e3d7e8f66cb8360e3b
Obinna Nwachukwu <75106868+OBIGOD3931@users.noreply.github.com>
2024-11-25 15:07:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -118,8 +118,7 @@ def app_main():
             # Hide menu, footer, and header
             hide_menu_style = """
                 <style>
-                #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;}
+               
                 header {visibility: hidden;}
                 #welcome-to-terrop{
                     color: #cfcebd;
