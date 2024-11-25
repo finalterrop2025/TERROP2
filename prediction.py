e7918ec86d924c9be8f0e37a996fc63c3a5fe3ef
Obinna Nwachukwu <75106868+OBIGOD3931@users.noreply.github.com>
2024-11-25 14:17:51 +0100

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -181,7 +181,7 @@ def app():
     
             if probability > 0.8:  
                 # Very high risk
-                st.markdown(f"<span style='color: #fc4245;'>The probability of a terrorist attack in {state} is: {probability:.2%}.</span>", unsafe_allow_html=True)
+                st.markdown(f"<span style='color: #fc4245; font-size:24px;'>The probability of a terrorist attack in {state} is: {probability:.2%}.</span>", unsafe_allow_html=True)
                 st.markdown("<div style='background-color: #e0d7d7; color: #e30505; border-radius:8px; padding: 15px;  text-align: justify;'>"
                                     "This is a very high-risk situation with low uncertainty. It is strongly recommended that the security agency take immediate and rigorous measures to ensure maximum security and mitigation of a possible attack."
                                     "</div>", unsafe_allow_html=True)
